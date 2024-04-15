7ca2b219dcbfd4ef60d7ce2d1b62846b590673b2
AnmolSonkar <anmolsonkar742@gmail.com>
2024-04-16 01:11:49 +0530

    docker added

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ app.get('/', async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
